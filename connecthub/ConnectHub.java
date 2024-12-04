c3cac362f8c21e65493c13938420f591c5bf6aa9
salma elshehy <salmaelshehy30@gmail.com>
2024-12-04 15:55:24 +0200

    profile managment

---
+++ connecthub/ConnectHub.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.connecthub;
+
+import FrorntEnd.profile;
+
+public class ConnectHub {
+
+    public static void main(String[] args) {
+         profile paint=new profile();
+        paint.show();
+    }
+}
